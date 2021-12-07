ae73817b850c1e4032825ea1631554935f7577ac
Tom Howland <tomhowland9@gmail.com>
2021-12-07 19:09:15 +0000

    Add MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import React, { useEffect } from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/theme.ts
@@ -0,0 +1,62 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      '50': '#C9EEEA',
+      light: '#00C7B7',
+      main: '#00AD9F',
+      dark: '#15847B',
+      '900': '#0D544E',
+    },
+    secondary: {
+      '50': '#F7F8F8',
+      light: '#E9EBEB',
+      main: '#A3A9AC',
+      dark: '#7D8589',
+      '900': '#2D3B41',
+    },
+    grey: {
+      '50': '#F7F8F8',
+      '300': '#E9EBEB',
+      '500': '#A3A9AC',
+      '700': '#7D8589',
+      '900': '#2D3B41',
+    },
+    error: {
+      '50': '#FED7E2',
+      light: '#FC989F',
+      main: '#FA3946',
+      dark: '#DF111F',
+      '900': '#BE131E',
+    },
+    background: {
+      default: '#F7F8F8',
+      paper: '#FFFFFF',
+    },
+    text: {
+      primary: '#0E1E25',
+      secondary: '#F7F8F8',
+    },
+  },
+  typography: {
+    fontFamily:
+      '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif',
+    h1: {
+      fontSize: '2.75rem',
+      fontWeight: 300,
+    },
+    h2: {
+      fontSize: '2.25rem',
+      fontWeight: 400,
+    },
+    h3: {
+      fontSize: '1.75rem',
+      fontWeight: 400,
+    },
+    h4: {
+      fontSize: '1.25rem',
+      fontWeight: 400,
+    },
+  },
+});
